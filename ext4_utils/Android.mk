2eea8d25d594057bb0f335dcb819a13c2f6e9480
SferaDev <sferadev@gmail.com>
2013-10-28 16:52:34 +0100

    system_extras: Add make_ext4fs utility
    Add make_ext4fs into $OUT/Utilities needed for some devices like Galaxy S I9000
    
    Change-Id: Id9c9ce79282859542a1e44501b19594145d80275

--- ext4_utils/Android.mk
+++ ext4_utils/Android.mk
@@ -47,6 +47,25 @@ endif
 include $(BUILD_HOST_EXECUTABLE)
 
 
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := make_ext4fs_main.c
+LOCAL_MODULE := utility_make_ext4fs
+LOCAL_MODULE_STEM := make_ext4fs
+LOCAL_MODULE_TAGS := optional
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_MODULE_CLASS := UTILITY_EXECUTABLES
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
+LOCAL_UNSTRIPPED_PATH := $(PRODUCT_OUT)/symbols/utilities
+LOCAL_STATIC_LIBRARIES += \
+    libext4_utils_static \
+    libselinux \
+    libsparse_static \
+    libz \
+    libcutils \
+    libc
+
+include $(BUILD_EXECUTABLE)
+
 #
 # -- All host/targets excluding windows
 #
